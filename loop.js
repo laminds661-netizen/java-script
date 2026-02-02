ddca390516e80bd24b744515b633955825bc3e83
laminds661-netizen <laminds661@gmail.com>
2026-02-02 12:32:47 +0530

    abc

--- loop.js
+++ loop.js
@@ -57,26 +57,126 @@
 
 
 
-let j = 12; // start
+// let j = 12; // start
 
-do {
-    console.log("do while loop:", j);
-    j++; // change (condition)
-} while (j < 20); // condition check happens AFTER running code
-
-
-for (let k = 1; k <= 201; k++) {
-    console.log("Loop with break", k);
-    if (k === 30) {
-        break;
-    }
-}
+// do {
+//     console.log("do while loop:", j);
+//     j++; // change (condition)
+// } while (j < 20); // condition check happens AFTER running code
 
 
-// continue
-for (let a = 1; a <= 10; a++) {
-    if (a === 5) {
-        continue;
-    }
-    console.log(a);
-}
+// for (let k = 1; k <= 201; k++) {
+//     console.log("Loop with break", k);
+//     if (k === 30) {
+//         break;
+//     }
+// }
+
+
+// // continue
+// for (let a = 1; a <= 10; a++) {
+//     if (a === 5) {
+//         continue;
+//     }
+//     console.log(a);
+// }
+
+
+// example 1
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+// for(let i=1; i<=20; i++){
+//      if(i%2===0){
+//          console.log(i);
+//      } 
+//     }
+
+
+
+// let i= 1;
+
+// while(i<=15){ 
+//     if(i%2 !==0)
+//         { 
+//             console.log(i);
+//         } i++; 
+//     }
+
+
+//  for(let i=1; i<=10; i++)
+//     { 
+//         console.log(`5 x ${i}=${5*i}`); 
+//     }
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     sum += i;
+// }
+
+// console.log("Sum =", sum);
+
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// let num = Number(prompt("give a number"));
+
+// for (let i = 1; i <= num; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i, "is even");
+//     } else {
+//         console.log(i, "is odd");
+//     }
+// }
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+//     if (i % 7 === 0) {
+//         break;
+//     }
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// let count = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 1) {        
+//         console.log(i);
+//         count++;            
+//     }
+
+//     if (count === 5) {
+//         break;               
+//     }
+// }
